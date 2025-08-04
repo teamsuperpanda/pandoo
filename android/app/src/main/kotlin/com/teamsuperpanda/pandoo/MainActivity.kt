7df86f77aa2e6cc968590c1ae700cbcfaedde17c
Team Super Panda <60556240+teamsuperpanda@users.noreply.github.com>
2025-08-05 07:54:20 +1200

    edge to edge

--- android/app/src/main/kotlin/com/teamsuperpanda/pandoo/MainActivity.kt
+++ android/app/src/main/kotlin/com/teamsuperpanda/pandoo/MainActivity.kt
@@ -1,5 +1,12 @@
 package com.teamsuperpanda.pandoo
 
 import io.flutter.embedding.android.FlutterActivity
+import android.os.Bundle
+import androidx.core.view.WindowCompat
 
-class MainActivity: FlutterActivity() 
+class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        super.onCreate(savedInstanceState)
+    }
+} 
